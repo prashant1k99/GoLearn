581fb26bebbff857b3ba4fa967dd15af9128017b
Prashant Singh <prashantco111@gmail.com>
2024-08-14 15:23:42 +0530

    Switch

--- Switch.go
+++ Switch.go
@@ -40,6 +40,16 @@ func main() {
 
 	// A type switch compares types instead of values. You can use this to discover the type of an interface value. In this example, the variable t will have the type corresponding to its clause
 	whatAmI := func(i interface{}) {
-		
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
 	}
+	whatAmI(true) // I'm a bool
+	whatAmI(1) // I'm an int
+	whatAmI("hey") //Don't know type string
 }
