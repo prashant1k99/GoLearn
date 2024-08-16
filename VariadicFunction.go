9bc8cb8c1520cffb77d53233b9204d1eb0058fea
Prashant <prashantco111@gmail.com>
2024-08-16 16:36:29 +0530

    Variadic Functions

---
+++ VariadicFunction.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// Variadic functions can be called with any number of trailing arguments. For example, fmt.Println is a common variadic function
+
+// Here’s a function that will take an arbitrary number of ints as arguments.
+func sum(nums ...int) {
+	fmt.Println(nums, " ")
+
+	total := 0
+	// Within the function, the type of nums is equivalent to []int. We can call len(nums), iterate over it with range, etc.
+	for _, num := range nums {
+		total += num
+	}
+	fmt.Println("Sum:", total)
+}
+
+func main() {
+	// Variadic functions can be called in the usual way with individual arguments.
+	sum(1,2,3,4,5,6,7)
+	// Sum: 28
+
+	// If you already have multiple args in a slice, apply them to a variadic function using func(slice...) like this.
+	nums := []int{1,2,3,64756,34,53467,486,4345}
+	sum(nums...)
+	// Sum: 123094
+}
